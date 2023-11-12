bff73a43c10a58a64dc08167c9ba8236654354ba
Craig Craig <craig@craig.se>
2023-11-12 02:42:21 -0600

    Update Scanner.cs

--- WinToolbox/Registry/Scanner.cs
+++ WinToolbox/Registry/Scanner.cs
@@ -1,7 +1,6 @@
 ﻿namespace WinToolbox.Registry;
 
 using System.Runtime.Versioning;
-using CheeseyUtils;
 using Microsoft.Win32;
 
 internal static class Scanner
@@ -9,44 +8,56 @@ internal static class Scanner
     [SupportedOSPlatform("windows")]
     public static void DoScan()
     {
-        // TODO: Check for ghost apps in "Uninstall" registry key
-        var uninstallKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
-        if (uninstallKey == null) throw new Exception("Uninstall key not found.");
+        var uninstallKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+            ?? throw new Exception("Uninstall key not found.");
+
         foreach (var subKeyName in uninstallKey.GetSubKeyNames())
         {
             var subKey = uninstallKey.OpenSubKey(subKeyName);
             if (subKey is not null)
             {
-                CheckUninstallEntry(subKey);
+                if (CheckUninstallEntry(subKey))
+                {
+                    uninstallKey.DeleteSubKey(subKeyName, true);
+                }
             }
         }
     }
 
+    /// <summary>
+    /// Checks if the given uninstall entry is a ghost app.
+    /// </summary>
+    /// <param name="subKey"></param>
+    /// <returns>true if the entry is a ghost app, otherwise false</returns>
     [SupportedOSPlatform("windows")]
-    private static void CheckUninstallEntry(RegistryKey subKey)
+    private static bool CheckUninstallEntry(RegistryKey subKey)
     {
+        // TODO: Check if the entry is a ghost app
+
         var displayName = subKey.GetValue("DisplayName")?.ToString();
         if (!string.IsNullOrEmpty(displayName))
         {
-            Log.WriteLine(displayName);
+            //Log.WriteLine(displayName);
         }
 
         var displayIcon = subKey.GetValue("DisplayIcon")?.ToString();
         if (!string.IsNullOrEmpty(displayIcon))
         {
-            Log.WriteLine($"\t{displayIcon}");
+            //Log.WriteLine($"\t{displayIcon}");
         }
 
         var installLocation = subKey.GetValue("InstallLocation")?.ToString();
         if (!string.IsNullOrEmpty(installLocation))
         {
-            Log.WriteLine($"\t{installLocation}");
+            //Log.WriteLine($"\t{installLocation}");
         }
 
         var uninstallString = subKey.GetValue("UninstallString")?.ToString();
         if (!string.IsNullOrEmpty(uninstallString))
         {
-            Log.WriteLine($"\t{uninstallString}");
+            //Log.WriteLine($"\t{uninstallString}");
         }
+
+        return false; // TODO: Return true if the entry is a ghost app
     }
 }
